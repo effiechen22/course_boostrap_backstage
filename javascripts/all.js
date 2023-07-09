bda9d9396ad3a4bfbdd3c3f98504d3ce1bf8bdf8
effiechen22 <effiechen22@gmail.com>
2023-07-09 11:33:24 +0800

    完成章節練習

---
+++ javascripts/all.js
@@ -0,0 +1,7 @@
+      //側欄縮合功能
+      const toggleMenuBtn = document.querySelector('#toggle-btn');
+      const body =  document.querySelector('body');
+      toggleMenuBtn.addEventListener('click',(event)=>{
+         event.preventDefault();
+         body.classList.toggle('sidebar-toggled');
+      });
